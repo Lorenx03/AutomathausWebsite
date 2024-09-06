793fe1a8a7c62f0e446627b5e7cebf4e2bd9c8bd
Lorenx03 <lorenx.gt@gmail.com>
2024-09-06 18:44:43 +0200

    mobile

--- src/lib/components/ui/navbar.svelte
+++ src/lib/components/ui/navbar.svelte
@@ -1,40 +1,85 @@
 <script lang="ts">
+    import { Menu } from 'lucide-svelte';
+    import { SunMoon } from 'lucide-svelte';
+
     import Github from "lucide-svelte/icons/github";
     import Newspaper from "lucide-svelte/icons/newspaper";
     import BookText from "lucide-svelte/icons/book-text";
     import House from "lucide-svelte/icons/house";
 
-    import Button from "./button/button.svelte";
     import DarkmodeButton from "./darkmodeButton.svelte";
     import LogoAutomat from "../svg/logoAutomat.svelte";
+
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+
+    import { toggleMode } from "mode-watcher";
 </script>
 
-<nav class="fixed p-5 w-full flex items-center justify-between backdrop-blur-md bg-white/80 dark:bg-zinc-950/80 z-50">
+<nav class="fixed p-3 md:p-5 w-full flex items-center justify-between backdrop-blur-md bg-white/80 dark:bg-zinc-950/80 z-50">
     <div class="flex items-center space-x-3 ">
-         <LogoAutomat class="size-9"/>
-        <h2 class="text-2xl">
+         <LogoAutomat class="size-7 lg:size-9"/>
+        <h2 class="text-xl lg:text-2xl">
             Automathaus<span class="text-zinc-500/30">.dev</span>
         </h2>
     </div>
 
     
-    <div class="flex items-center space-x-3 ">
-        <Button variant="outline">
+    <div class="md:flex items-center space-x-3">
+        <Button variant="outline" class="hidden md:flex">
             <House class="mr-2 h-4 w-4"/>
             Home
         </Button>
-        <Button variant="outline">
+        <Button variant="outline" class="hidden md:flex">
             <BookText class="mr-2 h-4 w-4"/>
             Docs
         </Button>
-        <Button variant="outline">
+        <Button variant="outline" class="hidden md:flex">
             <Newspaper class="mr-2 h-4 w-4"/>
             News
         </Button>
-        <Button>
+        <Button class="hidden md:flex">
             <Github class="mr-2 h-4 w-4"/>
             Github
         </Button>
-        <DarkmodeButton />
+
+        <DarkmodeButton class="hidden md:flex" />
+
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild let:builder>
+                <Button builders={[builder]} variant="ghost" size="icon" class="md:hidden">
+                    <Menu class="size-7" />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content align="end">
+                <DropdownMenu.Item >
+                    <House class="mr-2 h-4 w-4"/>
+                    <span>Home</span>
+                </DropdownMenu.Item>
+                
+                <DropdownMenu.Item >
+                    <BookText class="mr-2 h-4 w-4"/>
+                    <span>Docs</span>
+                </DropdownMenu.Item>
+    
+                <DropdownMenu.Item >
+                    <Newspaper class="mr-2 h-4 w-4"/>
+                    <span>News</span>
+                </DropdownMenu.Item>
+    
+                <DropdownMenu.Item >
+                    <Github class="mr-2 h-4 w-4"/>
+                    <span>Github</span>
+                </DropdownMenu.Item>
+
+                <DropdownMenu.Separator />
+
+                <DropdownMenu.Item on:click={toggleMode}>
+                    <SunMoon class="mr-2 h-4 w-4"/>
+                    <span>Toggle theme</span>
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
     </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
     import { ArrowRight } from 'lucide-svelte';
 
-    import LogoAutomat from "$lib/components/svg/logoAutomat.svelte";
     import Button from "$lib/components/ui/button/button.svelte";
     import CurveThing from '$lib/components/svg/CurveThing.svelte';
+    import AnimAutomatLogo from '$lib/components/svg/animAutomatLogo.svelte';
 </script>
 
 <div class="w-full h-svh flex justify-center content-center overflow-hidden relative">
     <div class="flex flex-col items-center justify-center space-y-6">
-        <LogoAutomat class="size-60 "/>
+        <AnimAutomatLogo />
         <div class="text-center space-y-2">
-            <h1 class="text-6xl">Automathaus</h1>
+            <h1 class="text-5xl lg:text-6xl">Automathaus</h1>
             <p class="text-lg">Open source domotic system</p>
         </div>
         
@@ -20,13 +20,10 @@
         </Button>
     </div>
 
-    <!-- <CurveThing class="absolute -bottom-20 -left-20 w-[35rem] h-[30rem] scale-[1.5] -rotate-45 fill-zinc-700 dark:fill-zinc-600"/> -->
-    <!-- <CurveThing class="absolute -bottom-40 -left-40 w-[35rem] h-[30rem] scale-[1.8] -rotate-45 fill-zinc-800 dark:fill-zinc-500"/> -->
-    <CurveThing class="absolute -bottom-60 -left-60 w-[35rem] h-[30rem] scale-[2] -rotate-45 fill-zinc-900 dark:fill-zinc-400"/>
-    <CurveThing class="absolute -bottom-80 -left-80 w-[35rem] h-[30rem] scale-[2.2]  -rotate-45 fill-zinc-950 dark:fill-zinc-300"/>
 
-    <!-- <CurveThing class="absolute -top-20 -right-20 w-[35rem] h-[30rem] scale-[1.5] rotate-[135deg] fill-zinc-700 dark:fill-zinc-600"/> -->
-    <!-- <CurveThing class="absolute -top-40 -right-40 w-[35rem] h-[30rem] scale-[1.8] rotate-[135deg] fill-zinc-800 dark:fill-zinc-500"/> -->
-    <CurveThing class="absolute -top-60 -right-60 w-[35rem] h-[30rem] scale-[2] rotate-[135deg] fill-zinc-900 dark:fill-zinc-400"/>
-    <CurveThing class="absolute -top-80 -right-80 w-[35rem] h-[30rem] scale-[2.2] rotate-[135deg] fill-zinc-950 dark:fill-zinc-300"/>
+    <CurveThing class="absolute -bottom-60 -left-60 -rotate-45 fill-zinc-900 dark:fill-zinc-400 size-[20rem] lg:size-[35rem] scale-[2]" />
+    <CurveThing class="absolute -bottom-80 -left-80 -rotate-45 fill-zinc-950 dark:fill-zinc-300 size-[20rem] lg:size-[35rem] scale-[2.2]" />
+
+    <CurveThing class="absolute -top-60 -right-60 rotate-[135deg] fill-zinc-900 dark:fill-zinc-400 size-[20rem] lg:size-[35rem] scale-[2]" />
+    <CurveThing class="absolute -top-80 -right-80 rotate-[135deg] fill-zinc-950 dark:fill-zinc-300 size-[20rem] lg:size-[35rem] scale-[2.2]" />
 </div>
