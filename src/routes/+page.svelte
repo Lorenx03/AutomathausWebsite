a7e49818be5cf2c2fc15455e84c54b46a05959c5
Lorenx03 <lorenx.gt@gmail.com>
2024-10-30 00:40:41 +0100

    updated logo dims

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,21 @@
 </script>
 
 <div class="w-full min-h-svh flex items-center justify-center">
-    <div class="flex flex-col items-center justify-center space-y-6">
-        <AnimAutomatLogo />
-        <div class="text-center space-y-2">
-            <h1 class="text-5xl lg:text-6xl">Automathaus</h1>
-            <p class="text-lg">Open source domotic system</p>
+    <div class="flex flex-col items-center justify-center lg:space-y-6">
+        <div class="scale-75 lg:scale-100">
+            <AnimAutomatLogo />
         </div>
-        
-        <Button class="hover:scale-110 ease-in-out transition-transform	duration-300" href="https://github.com/Automathaus">
-            Get Started
-            <ArrowRight class="ml-2 h-5 w-5"/>
-        </Button>
+        <div class="flex flex-col items-center justify-center space-y-6">
+            <div class="text-center space-y-2">
+                <h1 class="text-5xl lg:text-6xl">Automathaus</h1>
+                <p class="text-lg">Open source domotic system</p>
+            </div>
+            
+            <Button class="hover:scale-110 ease-in-out transition-transform	duration-300" href="https://github.com/Automathaus">
+                Get Started
+                <ArrowRight class="ml-2 h-5 w-5"/>
+            </Button>
+        </div>
     </div>
 
     <CurveThingsScreen />
